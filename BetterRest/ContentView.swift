e780666b54128e2e3a61032ea87075cf910c1a24
Jonathan Go <gojonathan.dev@gmail.com>
2019-10-23 12:20:08 +0800

    Did the challenge to change the stepper to slider and removed the calculate button and added a computed property instead of a function.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -20,10 +20,7 @@ struct ContentView: View {
   var body: some View {
     NavigationView {
       Form {
-        VStack(alignment: .leading, spacing: 0) {
-          Text("When do you want to wake up?")
-            .font(.headline)
-        
+        Section(header: Text("⏰ When do you want to wake up?").font(.headline)) {
           DatePicker("Please enter a time",
                    selection: $wakeUp,
                    displayedComponents: .hourAndMinute)
@@ -31,35 +28,44 @@ struct ContentView: View {
             .datePickerStyle(WheelDatePickerStyle())
         }
         
-        VStack(alignment: .leading, spacing: 0) {
-          Text("Desired amount of sleep")
-            .font(.headline)
+        Section(header: Text("🛏 Desired amount of sleep").font(.headline)) {
           Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
             Text("\(sleepAmount, specifier: "%g") hours")
           }
         }
          
-        VStack(alignment: .leading, spacing: 0) {
-          Text("Daily coffee intake")
-            .font(.headline)
-        
+        Section(header: Text("☕️ Daily coffee intake").font(.headline)) {
+//          Picker("Coffe intake", selection: $coffeeAmount) {
+//              ForEach(1...20, id: \.self) { i in
+//                  Text("\(i) \(i == 1 ? "cup" : "cups")")
+//              }
+//          }
+//          .id("coffee")
+//          .labelsHidden()
+//          .pickerStyle(WheelPickerStyle())
           Stepper(value: $coffeeAmount, in: 1...20) {
-            if coffeeAmount == 1 {
-              Text("1 cup")
-            } else {
-              Text("\(coffeeAmount) cups")
-            }
+              if coffeeAmount == 1 {
+                  Text("1 cup")
+              }
+              else {
+                  Text("\(coffeeAmount) cups")
+              }
           }
         }
+        
+        Section(header: Text("Recommended bed time").font(.headline)) {
+            Text("\(calculateBedtime)")
+                .font(.title)
+        }
       }
-      .navigationBarTitle("BetterRest")
-      .navigationBarItems(trailing:
-        Button(action: calculateBetime) {
-          Text("Calculate")
-        })
-        .alert(isPresented: $showingAlert) {
-          Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-      }
+      .navigationBarTitle(Text("BetterRest").foregroundColor(.blue))
+//      .navigationBarItems(trailing:
+//        Button(action: calculateBedtime) {
+//          Text("Calculate")
+//        })
+//        .alert(isPresented: $showingAlert) {
+//          Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+//      }
     }
   }
   
@@ -70,13 +76,14 @@ struct ContentView: View {
     return Calendar.current.date(from: components) ?? Date()
   }
     
-  func calculateBetime() {
+  var calculateBedtime: String {
     let model = SleepCalculator()
     
     let components = Calendar.current.dateComponents([.hour, .minute], from:  wakeUp)
     let hour = (components.hour ?? 0) * 60 * 60
     let minute = (components.minute ?? 0) * 60
     
+    var message: String //for last challenge
     do {
       let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
       
@@ -85,14 +92,16 @@ struct ContentView: View {
       let formatter = DateFormatter()
       formatter.timeStyle = .short
       
-      alertMessage = formatter.string(from: sleepTime)
-      alertTitle = "Your ideal bedtime is..."
+      message = formatter.string(from: sleepTime)
+      //alertMessage = formatter.string(from: sleepTime)
+      //alertTitle = "Your ideal bedtime is..."
     } catch {
-      alertTitle = "Error"
-      alertMessage = "Sorry, there was a problem calculating your bedtime."
+      message = "Error calculating bedtime"
+      //alertTitle = "Error"
+      //alertMessage = "Sorry, there was a problem calculating your bedtime."
     }
-    
-    showingAlert = true
+    return message
+    //showingAlert = true
   }
 }
 
